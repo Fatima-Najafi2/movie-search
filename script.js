1b60d88c9494406099ce99ed1c108bcb339cb84d
Fatima-Najafi <156585827+Fatima-Najafi2@users.noreply.github.com>
2024-04-11 18:00:51 +0430

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ async function getMovie(movieName) {
                   <div class="movie-time">
                       <span><span class="time">${movie.Runtime}</span></span>
                       <span><span class="gan">${movie.Genre}</span></span>
-                      <span class="add-film " > <img src="image/Icon.png" alt="" id="add-film">
+                      <span class="add-film " > <img src="./Icon.png" alt="" id="add-film">
                               Watchlist</span>
                   </div>
                   <p class="movie-desc">${movie.Plot}</p>
